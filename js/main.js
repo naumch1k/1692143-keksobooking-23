056e594422ec16b8eea6251121b8743799f50e18
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-05-29 18:54:32 -0500

    Исправляет функцию getRandomFloatNumber

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ getRandomNumber(-2, 10);
 
 const getRandomFloatNumber = (min, max, digitsAfterPoint) =>
   (min >= 0 && min < max)
-    ? (Math.random() * (max - min) + min).toFixed(digitsAfterPoint)
-    : (Math.random() * max).toFixed(digitsAfterPoint);
+    ? Number((Math.random() * (max - min) + min).toFixed(digitsAfterPoint))
+    : Number((Math.random() * max).toFixed(digitsAfterPoint));
 
 getRandomFloatNumber(1.10, 1.15, DIGITS_AFTER_POINT);
