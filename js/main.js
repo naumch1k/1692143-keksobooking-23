978f09bf5ebcc65a60d4ff3b6f2c667b8ef58a2c
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-05-29 13:26:17 -0500

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const DIGITS_AFTER_POINT = 2;
+
+const getRandomNumber = (min, max) =>
+  (min >= 0 && min < max)
+    ? Math.round(Math.random() * (max - min)) + min
+    : Math.round(Math.random() * max);
+
+getRandomNumber(-2, 10);
+
+const getRandomFloatNumber = (min, max, digitsAfterPoint) =>
+  (min >= 0 && min < max)
+    ? (Math.random() * (max - min) + min).toFixed(digitsAfterPoint)
+    : (Math.random() * max).toFixed(digitsAfterPoint);
+
+getRandomFloatNumber(1.10, 1.15, DIGITS_AFTER_POINT);
