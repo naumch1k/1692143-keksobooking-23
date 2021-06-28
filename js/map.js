cba31de59c97755cb02c2055dff502122d2182e4
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-06-28 15:07:20 -0500

    исправляет пути

--- js/map.js
+++ js/map.js
@@ -13,12 +13,12 @@ const mapZoomLevel = 12;
 
 const iconSettings = {
   mainPin: {
-    iconUrl: './../img/main-pin.svg',
+    iconUrl: '../img/main-pin.svg',
     iconSize: [52, 52],
     iconAnchor: [26, 52],
   },
   similarPin: {
-    iconUrl: './../img/pin.svg',
+    iconUrl: '../img/pin.svg',
     iconSize: [40, 40],
     iconAnchor: [20, 40],
   },
